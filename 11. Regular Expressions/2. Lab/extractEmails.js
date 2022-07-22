132b3d5795bba41f51ef193dd782274ab85a65a5
RadoslavKolev <radoslav.kolev@abv.bg>
2022-07-22 21:52:01 +0300

    Deleted comment ot extractEmails

--- 11. Regular Expressions/2. Lab/extractEmails.js
+++ 11. Regular Expressions/2. Lab/extractEmails.js
@@ -1,6 +1,4 @@
 function extractEmails(input) {
-  // const emailPattern = /(?<user> [A-Za-z0-9]+([.\-_][A-Za-z0-9]+)?)@(?<host>[a-zA-Z\-]+\.[A-Za-z\-]+(\.[A-Za-z\-]+)?)/g;
-  
   const emailPattern = /(?<user> [A-Za-z0-9]+[A-Za-z0-9-_.]*)@(?<host>[a-zA-Z-]+\.[A-Za-z-]+(\.[A-Za-z-]*)?)\b/g;
 
   const validEmails = input.match(emailPattern)
